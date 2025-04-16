2f32e7e3c8b86341b9508305c6d20a269886f5bf
Umair Mubeen <umair.mubeenir@gmail.com>
2025-04-16 15:45:23 +0500

    TaxCalculator

--- TaxBuddy/TaxBuddyApp/urls.py
+++ TaxBuddy/TaxBuddyApp/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Login Page
-    path('contact-us', views.Contact, name='contact-us'),  # Login Page
+    path('', views.index, name='index'),  # Home Page
+    path('contact-us', views.Contact, name='contact-us'),  # Contact Page
+    path('TaxCalculator', views.TaxCalculator, name='TaxCalculator'),  # Tax Calculator Page
 
 ]
 if settings.DEBUG:
