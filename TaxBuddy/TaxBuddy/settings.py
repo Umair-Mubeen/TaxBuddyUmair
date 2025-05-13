c61a729fda6e69ad316acc31266ee2b71199dd55
Umair Mubeen <umair.mubeenir@gmail.com>
2025-05-13 12:20:49 +0500

    dashboard theme added

--- TaxBuddy/TaxBuddy/settings.py
+++ TaxBuddy/TaxBuddy/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'TaxBuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taxbuddyumair',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
+
 
 
 # Password validation
@@ -118,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'TaxBuddyApp' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
